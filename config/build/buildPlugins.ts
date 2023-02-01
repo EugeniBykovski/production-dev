b0b480beca51bd45bb5bd6ee0f2121b04f1e8b0a
Eugeni Bykovski <eugeni.bykovski@itexus.com>
2023-02-01 18:33:45 +0100

    add config folder

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,18 @@
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    })
+  ]
+}
