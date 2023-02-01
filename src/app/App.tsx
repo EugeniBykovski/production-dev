5e64f0fd5045b251d2e4cdb1aa89c2b7e16a334a
Eugeni Bykovski <eugeni.bykovski@itexus.com>
2023-02-01 21:34:00 +0100

    updated folders and files for FSD architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom"
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
-import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { useTheme } from "./theme/useTheme"
-import { classNames } from "./helpers/classNames/classNames"
+import { MainPage } from "pages/MainPage"
+import { AboutPage } from "pages/AboutPage"
+import { useTheme } from "app/providers/ThemeProvider"
+import { classNames } from "shared/lib/classNames/classNames"
 
 import './styles/index.scss'
 
@@ -20,8 +20,8 @@ const App = () => {
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-            <Route path={'/'} element={<MainPageAsync />} />
-            <Route path={'/about'} element={<AboutPageAsync />} />
+            <Route path={'/'} element={<MainPage />} />
+            <Route path={'/about'} element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from "app/providers/ThemeProvider";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import ThemeProvider from "./theme/ThemeProvider";
-import App from './App';
+import App from './app/App';
 
 render (
   <BrowserRouter>
