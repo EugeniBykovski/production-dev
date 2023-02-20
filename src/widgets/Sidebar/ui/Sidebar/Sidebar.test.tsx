779c6c23dd692de1abb160638bb2083eaf18d346
EugeniBykovski <bykovskieugeni@mail.ru>
2023-02-20 15:05:09 +0100

    fixed tests for Sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 
 describe('Button', () => {
   test('Test render sidebar', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
     expect(screen.getByTestId('sidebar')).toBeInTheDocument()
   });
 
   test('Test toggle', () => {
-    renderWithTranslation(<Sidebar />)
+    componentRender(<Sidebar />)
 
     const toggleBtn = screen.getByTestId('sidebar-toggle')
     expect(screen.getByTestId('sidebar-toggle')).toBeInTheDocument()
