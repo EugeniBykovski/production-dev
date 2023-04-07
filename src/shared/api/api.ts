2ed1cc32dd29901d106336919b5292a83894a7ed
EugeniBykovski <bykovskieugeni@mail.ru>
2023-04-07 14:07:41 +0200

    fixed api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
-  }
+})
+
+$api.interceptors.request.use(config => {
+  if (config.headers) config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  return config;
 })
